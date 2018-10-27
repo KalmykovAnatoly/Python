db3f250086389e2500498b242bdf8429f9105f84
Kalmykov <kalmikovanatoliy@mail.ru>
2018-10-28 00:27:56 +0300

    Finished 4

--- no_name.py
+++ no_name.py
@@ -1,8 +1,16 @@
+import math
+
+import numpy as np
 import pandas as pd
-import quandl, math, config
+import quandl
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
+
+import config
 
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 5000)
 quandl.ApiConfig.api_key = config.KEY.get('api_key')
 
 df = quandl.get('WIKI/GOOGL')
@@ -15,9 +23,19 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df)))
-print(forecast_out)
+forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
-print(df.head())
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+# clf = svm.SVR()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
