ff50d0ea8d887e5d8e1fd31e8c337b749ef87e08
Kalmykov <kalmikovanatoliy@mail.ru>
2018-10-28 14:06:32 +0300

    Pickling classifier

--- no_name.py
+++ no_name.py
@@ -1,13 +1,17 @@
-import math
+import math, datetime
 
 import numpy as np
 import pandas as pd
+import pickle
 import quandl
-from sklearn import preprocessing, model_selection, svm
+from sklearn import preprocessing, model_selection
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
 
 import config
 
+style.use('ggplot')
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 5000)
@@ -26,16 +30,43 @@ df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
 clf = LinearRegression(n_jobs=-1)
 # clf = svm.SVR()
 clf.fit(X_train, y_train)
+with open('linearregression.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
 accuracy = clf.score(X_test, y_test)
-print(accuracy)
+forecast_set = clf.predict(X_lately)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
