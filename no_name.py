9f5152ecfed6b32fd6f21c84efb993b1491eb758
Kalmykov <kalmikovanatoliy@mail.ru>
2018-11-07 00:37:12 +0300

    Ended nearest neighborn

--- no_name.py
+++ no_name.py
@@ -27,11 +27,11 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.01 * len(df)))
+forecast_out = int(math.ceil(0.1 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-X = np.array(df.drop(['label'], 1))
+X = np.array(df.drop(['label', 'HL_PCT', 'PCT_change', 'Adj. Volume'], 1))
 X = preprocessing.scale(X)
 X = X[:-forecast_out]
 X_lately = X[-forecast_out:]
