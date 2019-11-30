3622775e14f0872b796ca8baecb8774dfae24750
Kalmykov <kalmikovanatoliy@mail.ru>
2019-12-01 00:59:46 +0300

    started mnist lenet

---
+++ stepik/c_vision/mnist_lenet.py
@@ -0,0 +1,71 @@
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision.datasets
+
+random.seed(0)
+np.random.seed(0)
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+torch.backends.cudnn.determenistic = True
+
+MNIST_train = torchvision.datasets.MNIST('./', download=True, train=True)
+MNIST_test = torchvision.datasets.MNIST('./', download=True, train=False)
+
+X_train = MNIST_train.data
+y_train = MNIST_train.targets
+X_test = MNIST_test.data
+y_test = MNIST_test.targets
+
+plt.imshow(X_train[0, :, :])
+plt.show()
+print(y_train[0])
+
+X_train = X_train.unsqueeze(1).float()
+X_test = X_test.unsqueeze(1).float()
+
+
+class LeNet(torch.nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=1, out_channels=6, kernel_size=5, padding=2
+        )
+        self.act1 = torch.nn.Tanh()
+        self.pool1 = torch.nn.AvgPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = torch.nn.Conv2d(
+            in_channels=6, out_channels=16, kernel_size=5, padding=0
+        )
+        self.act2 = torch.nn.Tanh()
+        self.pool2 = torch.nn.AvgPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = torch.nn.Linear(5 * 5 * 16, 120)
+        self.act3 = torch.nn.Tanh()
+
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.act4 = torch.nn.Tanh()
+
+        self.fc3 = torch.nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.act1(x)
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = self.act2(x)
+        x = self.pool2(x)
+
+        x = x.view(x.size(0), x.size(1) * x.size(2) * x.size(3))
+
+        x = self.fc1(x)
+        x = self.act3(x)
+        x = self.fc2(x)
+        x = self.fc4(x)
+        x = self.fc3(x)
+
+        return x;
